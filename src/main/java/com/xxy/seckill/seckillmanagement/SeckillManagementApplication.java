29d7d39c962c33c82f92236ad615e218fdc9b895
苍穹勇敢的心 <18825237023@163.com>
2019-11-18 16:59:01 +0800

    Create SeckillManagementApplication.java

---
+++ src/main/java/com/xxy/seckill/seckillmanagement/SeckillManagementApplication.java
@@ -0,0 +1,111 @@
+package com.xxy.seckill.seckillmanagement;
+
+import com.xxy.seckill.seckillmanagement.dao.UserDAOMapper;
+import com.xxy.seckill.seckillmanagement.dataobject.UserDAO;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.util.ClassUtils;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.xml.ws.Action;
+import java.io.File;
+import java.io.InputStream;
+import java.lang.reflect.Method;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@SpringBootApplication
+@RestController
+@MapperScan("com.xxy.seckill.seckillmanagement.dao")
+public class SeckillManagementApplication {
+    @Autowired
+    private UserDAOMapper userDAOMapper;
+
+    private static String accessType;
+
+    private static String accessHost;
+
+    private static String port;
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SeckillManagementApplication.class, args);
+        String loginAddress = getAddress() + "/login";
+        browse(loginAddress);
+    }
+
+    @RequestMapping("/")
+    public String home() {
+        UserDAO userDAO = userDAOMapper.selectByPrimaryKey(1);
+        if (null == userDAO) {
+            return "用户对象不存在";
+        } else {
+            return userDAO.getName();
+        }
+    }
+
+    /**
+     * 使用默认浏览器打开url
+     *
+     * @param url 打开文件的路径
+     * @throws Exception
+     */
+    private static void browse(String url) throws Exception {
+        // 获取操作系统的名字
+        String osName = System.getProperty("os.name", "");
+        if (osName.startsWith("Windows")) {
+            // windows
+            Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + url);
+        } else if (osName.startsWith("Mac OS")) {
+            // Mac
+            Class fileMgr = Class.forName("com.apple.eio.FileManager");
+            Method openURL = fileMgr.getDeclaredMethod("openURL", String.class);
+            openURL.invoke(null, url);
+        } else {
+            // Unix or Linux
+            String[] browsers = {"firefox", "opera", "konqueror", "epiphany", "mozilla", "netscape"};
+            String browser = null;
+            // 执行代码，在brower有值后跳出，
+            for (int count = 0; count < browsers.length && browser == null; count++) {
+                // 这里是如果进程创建成功了，==0是表示正常结束。
+                if (Runtime.getRuntime().exec(new String[]{"which", browsers[count]}).waitFor() == 0) {
+                    browser = browsers[count];
+                }
+            }
+            if (browser == null) {
+                throw new RuntimeException("未找到任何可用的浏览器");
+            } else {
+                // 这个值在上面已经成功的得到了一个进程。
+                Runtime.getRuntime().exec(new String[]{browser, url});
+            }
+        }
+    }
+
+    private static String getAddress() throws UnknownHostException {
+        InetAddress address = InetAddress.getLocalHost();
+        String host = accessHost == null ? address.getHostAddress() : accessHost;
+        String path = accessType + host + ":" + port;
+        return path;
+    }
+
+    @Value("${server.port}")
+    public void setPort(String port) {
+        this.port = port;
+    }
+
+    @Value("${access.type}")
+    public void setAccessType(String accessType) {
+        this.accessType = accessType;
+    }
+
+    @Value("${access.host}")
+    public void setAccessHost(String accessHost) {
+        this.accessHost = accessHost;
+    }
+}
